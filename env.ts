27c0358ea74e57b3170e25b1b37b3c09462deaeb
nkfr26 <n@rfu.jp>
2025-04-30 00:17:50 +0900

    fix

--- env.ts
+++ env.ts
@@ -3,14 +3,8 @@ import { z } from "zod";
 
 config({ path: ".env" });
 
-export const envVariables = z.object({
+export const envSchema = z.object({
   DATABASE_URL: z.string(),
 });
 
-export const env = envVariables.parse(process.env);
-
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv extends z.infer<typeof envVariables> {}
-  }
-}
+export const env = envSchema.parse(process.env);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import { userExists } from "./packages/domain/user/service/user-exists";
 import {
+  deleteUserCommand,
   insertUserCommand,
   selectAllUsersQuery,
+  selectUserByIdQuery,
   selectUserByNameQuery,
+  updateUserCommand,
 } from "./packages/infrastructure/user";
+import { deleteUser } from "./packages/use-case/user/delete-user";
 import { getAllUsers } from "./packages/use-case/user/get-all-users";
 import { registerUser } from "./packages/use-case/user/register-user";
+import { updateUserName } from "./packages/use-case/user/update-user-name";
 
 const main = async () => {
   await registerUser(
@@ -25,26 +30,26 @@ const main = async () => {
     );
   });
 
-  // await updateUserName(
-  //   selectUserByIdQuery,
-  //   userExists(selectUserByNameQuery),
-  //   updateUserCommand,
-  // )("01966230-8abb-71ca-80f2-0f127a1c4cf0", "fuga").then((result) => {
-  //   result.match(
-  //     (value) => console.log(value),
-  //     (error) => console.error(error),
-  //   );
-  // });
+  await updateUserName(
+    selectUserByIdQuery,
+    userExists(selectUserByNameQuery),
+    updateUserCommand,
+  )("01966230-8abb-71ca-80f2-0f127a1c4cf0", "fuga").then((result) => {
+    result.match(
+      (value) => console.log(value),
+      (error) => console.error(error),
+    );
+  });
 
-  // await deleteUser(
-  //   selectUserByIdQuery,
-  //   deleteUserCommand,
-  // )("01966230-8abb-71ca-80f2-0f127a1c4cf0").then((result) => {
-  //   result.match(
-  //     (value) => console.log(value),
-  //     (error) => console.error(error),
-  //   );
-  // });
+  await deleteUser(
+    selectUserByIdQuery,
+    deleteUserCommand,
+  )("01966230-8abb-71ca-80f2-0f127a1c4cf0").then((result) => {
+    result.match(
+      (value) => console.log(value),
+      (error) => console.error(error),
+    );
+  });
 };
 
 main();
