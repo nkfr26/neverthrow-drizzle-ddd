7f249ce6d6580ce7d758f72b69c9953b519e7b02
nkfr26 <n@rfu.jp>
2025-05-04 02:31:20 +0900

    fix

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,7 +2,7 @@ import { pgTable, text, uuid } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   userId: uuid("user_id").primaryKey(),
-  name: text("name").notNull(),
+  name: text("name").notNull().unique(),
 });
 
 export type UserDataModel = typeof usersTable.$inferSelect;
